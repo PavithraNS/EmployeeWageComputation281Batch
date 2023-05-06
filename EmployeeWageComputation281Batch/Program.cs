db08011c8016b0cfc7fc92372a1c0aaa00f220cd
PavithraNS <nspavi1991@gmail.com>
2023-05-06 12:59:07 +0530

    [Pavithra] Modify Checking Empkoyee is present or not

--- EmployeeWageComputation281Batch/Program.cs
+++ EmployeeWageComputation281Batch/Program.cs
@@ -8,10 +8,22 @@ namespace EmployeeWageComputation281Batch
 {
     class Program
     {
-         static void Main(string[] args)
+        static void Main(string[] args)
         {
+            const int IS_PRESENT=1;
             Console.WriteLine("Welcome to EmployeeWageCompute");
+            Random random = new Random();
+            int randomInput=random.Next(0,2); //0 or 1
+            if (IS_PRESENT== randomInput)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
             Console.ReadLine();
         }
+
     }
 }
