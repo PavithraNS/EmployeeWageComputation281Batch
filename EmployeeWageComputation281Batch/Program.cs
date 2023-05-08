ee5b01a7e6d766a6850672e1c05b42f5fcf7ea2a
PavithraNS <nspavi1991@gmail.com>
2023-05-08 11:41:19 +0530

    [Pavithra] Modify Calculating employee wage

--- EmployeeWageComputation281Batch/Program.cs
+++ EmployeeWageComputation281Batch/Program.cs
@@ -10,18 +10,24 @@ namespace EmployeeWageComputation281Batch
     {
         static void Main(string[] args)
         {
-            const int IS_PRESENT=1;
+            const int FULL_TIME=1;
+            const int EMP_RATE_PER_HR = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Console.WriteLine("Welcome to EmployeeWageCompute");
             Random random = new Random();
             int randomInput=random.Next(0,2); //0 or 1
-            if (IS_PRESENT== randomInput)
+            if (FULL_TIME == randomInput)
             {
+                empHrs = 8;
                 Console.WriteLine("Employee is present");
             }
             else
             {
                 Console.WriteLine("Employee is absent");
             }
+            empWage = EMP_RATE_PER_HR * empHrs;
+            Console.WriteLine("Employee Wage per day:{0}",empWage);
             Console.ReadLine();
         }
 
